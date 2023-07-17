43130b4f4b9db7b3f224708a47ac76b70a564f75
YK <130263480+HARUHARU0220@users.noreply.github.com>
2023-07-17 17:47:47 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,13 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const fs = require("fs");
-const xlsx = require("xlsx");
+const puppeteer = require('puppeteer');
 
+(async () => {
+    const browser = await puppeteer.launch({headless:false});
+    const page = await browser.newPage();
+    await page.goto('https://apply.jinhakapply.com/SmartRatio');
 
+    const links = await page.$$eval('a.rate', allA =>
+    allA.map(a => a.href));
 
-const getHTML = async(keyword) =>{
-    try {
-        return await axios.get("https://www.inflearn.com/courses?s=" + encodeURI(keyword))
-    } catch(err) {
-        console.log(err)
+    console.log(links);
 
-    }
-
-}
-
-const parsing = async (keyword) => {
-    const html = await getHTML(keyword);
-    const $ = cheerio.load(html.data); // 제이쿼리
-    const $courseList = $(".course_card_item");
-
-    let courses = [];
-    $courseList.each((idx,node) => {
-        const title = $(node).find(".course_title").text();
-        courses.push({
-            title: $(node).find(".course_title").text(),
-            instructor: $(node).find(".instructor").text(),
-            price: $(node).find(".price").text(),
-            rating: $(node).find(".star_solid").css("width") // 제이쿼리 문법과 동일함
-        })
-    });
-
-    const workbook = xlsx.utils.book_new();
-    const worksheet = xlsx.utils.json_to_sheet(courses);
-    xlsx.utils.book_append_sheet(workbook, worksheet, "Courses");
-    const excelFileName = "courses.xlsx";
-    xlsx.writeFile(workbook, excelFileName);
-    console.log("Data extracted to", excelFileName);
-};
-
-parsing("자바스크립트");
+})();
