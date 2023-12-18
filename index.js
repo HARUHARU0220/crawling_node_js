ba9e7d44408fe3bb9d49c475c1f291c1541fafdc
Yukyung <130263480+HARUHARU0220@users.noreply.github.com>
2023-12-18 21:10:50 +0900

    가천대 경쟁률 크롤링 코드

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const puppeteer = require('puppeteer');
+const fs = require('fs');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const json2xls = require('json2xls');
 
-(async () => {
-    const browser = await puppeteer.launch({headless:false});
-    const page = await browser.newPage();
-    await page.goto('http://addon.jinhakapply.com/RatioV1/RatioH/Ratio10190351.html');
+const API = 'http://addon.jinhakapply.com/RatioV1/RatioH/Ratio10190351.html';
 
-    const links = await page.$$eval('a.rate', allA =>
-        allA.map(a => a.href));
+const scrapperScript = async () => {
+    try {
+        const { data } = await axios.get(API);
+        const $ = cheerio.load(data);
 
-    console.log(links);
+        const scrapedData = [];
 
-})();
+        // 선택자 '.unit'에 해당하는 요소들을 추출하여 데이터 저장
+        $('.unit').each((index, el) => {
+            const major = $(el).text();
+            scrapedData.push({ major });
+        });
+
+        // 선택자 '.rate4'에 해당하는 요소들을 추출하여 데이터 저장
+        $('.rate4').each((index, el) => {
+            const rate4 = $(el).text();
+            scrapedData[index].ratio= rate4;
+        });
+
+        console.dir(scrapedData); // console.dir 경우엔 JSON과 같은 트리 구조로 출력
+
+        const xls = json2xls(scrapedData); // json2xls로 JSON 데이터를 엑셀 파일 변환
+
+        fs.writeFileSync('scrapedData.xlsx', xls, 'binary'); // 변환된 데이터를 'scrapedData.xlsx'라는 엑셀 파일 저장
+        console.log('데이터가 엑셀 파일로 저장되었습니다.');
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+scrapperScript();
